1f7291d8ff6db40645ecc5eea332ec8001de9ae7
Lawrence <ganjeblerence@gmail.com>
2026-03-21 03:06:26 +0200

    fixed some env naming issues

--- firebase-applet-config.js
+++ firebase-applet-config.js
@@ -2,11 +2,11 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  projectId: import.meta.env.VIT_FIREBASE_PROJECT_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  apiKey: import.meta.env.VIT_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VIT_FIREBASE_AUTH_DOMAIN,
-  firestoreDatabaseId: import.meta.env.VIT_FIRESTORE_DATABASE_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  firestoreDatabaseId: import.meta.env.VITE_FIRESTORE_DATABASE_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
